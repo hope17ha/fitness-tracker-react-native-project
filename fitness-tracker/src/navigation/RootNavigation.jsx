de68e1aad00b2eaf0b92bf062840c88dff0a908a
hope17ha <n.anastasova91@gmail.com>
2026-02-09 16:23:43 +0200

    config: json server

--- fitness-tracker/src/navigation/RootNavigation.jsx
+++ fitness-tracker/src/navigation/RootNavigation.jsx
@@ -1,6 +1,6 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import LoginScreen from '../screens/LoginScreen.jsx';
-import SplashScreen from '../screens/SplashScree.jsx';
+import SplashScreen from '../screens/SplashScreen.jsx';
 
 const Stack = createNativeStackNavigator();
 
@@ -11,7 +11,7 @@ export default function RootNavigation() {
     return (
                 
                 <Stack.Navigator>
-                    <Stack.Screen name="Splash Screen" component={SplashScreen} options={{headerShown: false}} /> 
+                    <Stack.Screen name="Splash Screen" component={SplashScreen} options={{ headerShown: false }} /> 
                     <Stack.Screen name="Login Screen" component={LoginScreen} options={ { headerShown: false }}/>
                 </Stack.Navigator>
         
