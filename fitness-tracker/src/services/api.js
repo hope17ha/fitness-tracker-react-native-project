3fc1462be1b72b9bbea440f020d61efe01b76125
hope17ha <n.anastasova91@gmail.com>
2026-03-02 13:28:52 +0200

    fix: env and scripts

--- fitness-tracker/src/services/api.js
+++ fitness-tracker/src/services/api.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 
 export const api = axios.create({
-    baseURL: 'http://192.168.1.4:3001'
+    baseURL: process.env.EXPO_PUBLIC_API_URL,
+    headers: { "Content-Type": "application/json" },
 })
